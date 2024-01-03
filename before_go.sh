236c71427b42056f92be981a20c544b393e32eea
Ki-wimon <youtinlin@gmail.com>
2024-01-03 12:32:24 +0800

    update before_go.sh

--- before_go.sh
+++ before_go.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "running: sudo apt install libncurses5-dev"
+sudo apt install libncurses5-dev
+
 cd ./sl
 go mod init sl
 go mod tidy
@@ -14,3 +17,6 @@ go mod init ${PWD##*/}
 go mod edit -replace sl=./sl
 go mod edit -replace cmdutil=./cmdutil
 go mod tidy
+
+file_path=$(go env GOMODCACHE)/seehuhn.de/go/ncurses@v0.2.0/keys.go
+sudo sed -i "171s/.*/  	\/\/ C.KEY_EVENT:     KeyEvent,/" "$file_path"
